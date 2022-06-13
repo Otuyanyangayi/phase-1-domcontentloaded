7c944b5200a55a829fc49e43e2cef86015c67d4c
Grace <gracenyangayi@gmail.com>
2022-06-13 17:05:24 +0300

    commit tree d995eea9efb40661c922c7b71b5a9d3a642d2fc1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  updateP()
+
+  });
+  function updateP(){
+    document.getElementById("text").textContent = "This is really cool!"
+  }
+  
